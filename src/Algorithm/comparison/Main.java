8f436ad13e54d8f1264b4c1757bdd83a13e55230
stduyhh <ghls1252@gmail.com>
2025-05-02 14:46:54 +0900

    kiosk

---
+++ src/Algorithm/comparison/Main.java
@@ -0,0 +1,13 @@
+package Algorithm.comparison;
+
+import Algorithm.Solution;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution =new Solution();
+        int x = solution.solution(1,2);
+        int y= solution.solution(1,1);
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
---
+++ src/Algorithm/comparison/Solution.java
@@ -0,0 +1,11 @@
+package Algorithm.comparison;
+
+public class Solution {
+    public int solution(int num1, int num2){
+        if (num1 == num2){
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+}
