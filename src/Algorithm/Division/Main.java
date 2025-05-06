ddcd6a7b68852a04d529f2ad22110bfd57879ce9
stduyhh <ghls1252@gmail.com>
2025-05-06 14:07:51 +0900

    division

---
+++ src/Algorithm/Division/Main.java
@@ -0,0 +1,10 @@
+package Algorithm.Division;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int x = solution.solution(200, 2);
+        System.out.println(x);
+    }
+}
---
+++ src/Algorithm/Division/Solution.java
@@ -0,0 +1,8 @@
+package Algorithm.Division;
+
+public class Solution {
+    public int solution(int num1, int num2){
+        int answer = num1 / num2;
+        return answer;
+    }
+}
